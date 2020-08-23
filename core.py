841e50f95fc3e5cfbad7129b645aa5c1f0339849
Biryukov-lang98 <70016925+Biryukov-lang98@users.noreply.github.com>
2020-08-23 22:18:56 +0300

    Update core.py

--- core.py
+++ core.py
@@ -7,15 +7,16 @@ bot = telebot.TeleBot(config.token)
 URL = 'https://sinoptik.com.ru/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D0%BA%D1%80%D0%B0%D1%81%D0%BD%D0%BE%D0%B4%D0%B0%D1%80'
 HEADERS = {'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36', 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'}
 
-global t_min, t_max
-
 def get_html(url, params = None):
     r = requests.get(url, headers = HEADERS, params = params)
     return r
 
 def get_content(html):
+  global t_min, t_max  
+
     soup = BS(html, 'html.parser')
     items = soup.select('div', "#sinoptik-app > div.wrapper > main > div.weather__content > div.weather__content_tabs.clearfix > div.weather__content_tab.current")
+    
     for item in items.select('.weather__content_tab-temperature'):
         t_min =  item.select(' .min')[0].text
         t_max =  item.select(' .max')[0].text
